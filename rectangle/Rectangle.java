4297655f848d5015c687c3f3788f1faacd307a7a
rayenzinoubi <147351828+rayenzinoubi@users.noreply.github.com>
2023-12-06 17:05:29 +0100

    Add files via upload

---
+++ rectangle/Rectangle.java
@@ -0,0 +1,12 @@
+public class Rectangle {float largeur, longeur;
+    float diagonale(float a, float b){
+        return(a * a + b * b);
+    }
+    float perimetre(float a, float b){
+        return (a * 2 + b * 2);
+    }
+
+    float doubler(float a, float b){
+        return (a * 2);
+    }
+}
